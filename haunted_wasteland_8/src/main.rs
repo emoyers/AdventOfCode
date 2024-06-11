b2b3fd5286199062efc9616ae43e0f43d6df1979
Emilio Moyers <emoyersb@gmail.com>
2024-06-11 08:04:25 -0700

    passing part 2 of quest 8

--- haunted_wasteland_8/src/main.rs
+++ haunted_wasteland_8/src/main.rs
@@ -63,7 +63,7 @@
 
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use regex::Regex;
 
 #[derive(Debug)]
@@ -74,6 +74,7 @@ enum TypeRun {
 
 fn main() -> std::io::Result<()> {
     algorithm(TypeRun::FirstPart)?;
+    algorithm(TypeRun::SecondPart)?;
 
     Ok(())
 }
@@ -84,7 +85,7 @@ fn algorithm(type_run: TypeRun) -> std::io::Result<()> {
     let reader: BufReader<File> = BufReader::new(file);
 
     let mut network_data: NetworkData = NetworkData::new();
-    let re = Regex::new(r"^[A-Z]{3} = \([A-Z]{3}, [A-Z]{3}\)$").unwrap();
+    let re = Regex::new(r"^[A-Z0-9]{3} = \([A-Z0-9]{3}, [A-Z0-9]{3}\)$").unwrap();
 
     for (i,line) in reader.lines().enumerate() {
 
@@ -101,21 +102,42 @@ fn algorithm(type_run: TypeRun) -> std::io::Result<()> {
     }
 
     // init the locations and destinations
-    network_data.init_single_location_destination();
+    match type_run {
+        TypeRun::FirstPart => network_data.init_single_location_destination(),
+        TypeRun::SecondPart => network_data.init_multiple_location_destination(),
+    }
 
-    let num_steps: u64 = network_data.get_number_steps();
+    let num_steps: u64 = network_data.get_number_steps(&type_run);
     println!("The number of steps to reach the destination for {:?} is: {}", type_run, num_steps);
 
     Ok(())
 }
 
 #[derive(Debug)]
+struct NodeVisitedInfo {
+    ready: bool,
+    numbers_steps: u64,
+    delta_steps_from_last_time: u64
+}
+
+impl NodeVisitedInfo {
+    fn new() -> Self {
+        NodeVisitedInfo{
+            ready : false,
+            numbers_steps : 0,
+            delta_steps_from_last_time : 0,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct NetworkData {
     init_locations: Vec<String>,
-    goal_locations: Vec<String>,
+    goal_locations: HashSet<String>,
     current_locations: Vec<String>,
     directions: Vec<char>,
     network_map: HashMap<String, (String, String)>,
+    important_node_visited: HashMap<String, NodeVisitedInfo>,
 }
 
 impl NetworkData {
@@ -123,17 +145,47 @@ impl NetworkData {
     fn new() -> Self {
         NetworkData{
             init_locations: Vec::new(),
-            goal_locations: Vec::new(),
+            goal_locations: HashSet::new(),
             current_locations: Vec::new(),
             directions: Vec::new(),
             network_map: HashMap::new(),
+            important_node_visited : HashMap::new(),
         }
     }
 
     fn init_single_location_destination(& mut self) {
         self.init_locations.push("AAA".to_string());
-        self.goal_locations.push("ZZZ".to_string());
-        
+        self.goal_locations.insert("ZZZ".to_string());
+
+        self.init_current_locations();
+
+    }
+
+    fn init_multiple_location_destination(& mut self) {
+
+        for (key, _) in self.network_map.iter() {
+            
+            let key_chars: Vec<char> = key.chars().collect();
+            
+            if key_chars.len() > 2 {
+                if key_chars[2] == 'A' {
+                    self.init_locations.push(key.clone());   
+                }
+                else if key_chars[2] == 'Z' {
+                    self.goal_locations.insert(key.clone());
+                }
+            }
+        }
+
+        // Init important nodes to visit
+        for loc in self.goal_locations.iter() {
+            self.important_node_visited.insert(loc.clone(), NodeVisitedInfo::new());
+        }
+
+        self.init_current_locations();
+    }
+
+    fn init_current_locations(& mut self) {
         for loc in &self.init_locations {
             self.current_locations.push(loc.clone());
         }
@@ -159,11 +211,10 @@ impl NetworkData {
         }
     }
 
-    fn get_number_steps(& mut self) -> u64{
+    fn get_number_steps(& mut self, type_run: &TypeRun) -> u64{
         let mut number_steps: u64 = 0;
         let mut directions_index: usize = 0;
         let mut continue_flag: bool = true;
-
 
         while continue_flag {
             
@@ -181,19 +232,98 @@ impl NetworkData {
             directions_index = (directions_index + 1) % self.directions.len();
             number_steps += 1;
 
-            continue_flag = !self.all_locations_reach_destination(); 
+            continue_flag = !self.all_locations_reach_destination();
+
+            if matches!(type_run, TypeRun::SecondPart){
+                self.update_important_nodes_visited(number_steps);
+                continue_flag &= !self.can_calculate_intersection();
+            }
         }
+
+        if !self.all_locations_reach_destination() && self.can_calculate_intersection(){
+            return self.estimate_number_steps();
+        }  
+            
+        
         number_steps
     }
 
     fn all_locations_reach_destination(&self) -> bool{
         let mut reach_location : bool = true;
-        for (i, goal_loc) in self.goal_locations.iter().enumerate() {
-            if goal_loc != &self.current_locations[i] {
+        for cur_loc in self.current_locations.iter() {
+            if !self.goal_locations.contains(cur_loc) {
                 reach_location = false;
                 break;
             }
         }
         reach_location
+    }
+
+    fn can_calculate_intersection(&self) -> bool {
+        let mut result: bool = true;
+        for (_, val) in &self.important_node_visited {
+            if !val.ready {
+                result = false;
+                break;
+            }
+        }
+        result
+    }
+
+    fn update_important_nodes_visited(&mut self, steps: u64) {
+        for loc in &self.current_locations {
+            match self.important_node_visited.get_mut(loc) {
+                Some(x) => {
+                    if !x.ready {
+                        let delta = steps - x.numbers_steps;
+                        x.ready = if delta == x.delta_steps_from_last_time {true} else {false};
+                        x.delta_steps_from_last_time = delta;
+                        x.numbers_steps = if !x.ready {steps} else {x.numbers_steps};
+                    }
+                },
+                None => continue,
+            }
+        }
+    }
+
+    fn estimate_number_steps(&mut self) -> u64 {
+        let mut important_nodes_steps: Vec<u64> = Vec::new();
+        
+        if self.can_we_used_lcm() {
+            for (_, node) in &self.important_node_visited {
+                important_nodes_steps.push(node.numbers_steps.clone());
+            }
+            Self::least_common_multiple(&important_nodes_steps)
+        }
+        else {
+            println!("Develop new more complex algorithm!");
+            0
+        }
+
+        
+    }
+
+    fn can_we_used_lcm(& self) -> bool{
+        let mut result: bool = true;
+
+        for (_, node) in &self.important_node_visited {
+            if node.numbers_steps != node.delta_steps_from_last_time {
+                result = false;
+                break;
+            }
+        }
+        
+        result
+    }
+
+    fn greateast_common_divisor(a: u64, b: u64) -> u64 {
+        if b == 0 {
+            return a;
+        }
+        Self::greateast_common_divisor(b, a % b)
+    }
+
+    fn least_common_multiple(nums: &Vec<u64>) -> u64 {
+        nums.iter().fold(1, |a, &b| a * b / Self::greateast_common_divisor(a, b))
     }
 }
